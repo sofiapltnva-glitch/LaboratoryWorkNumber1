546a15e3f0b4a0e50a08ea2e4541db039788f32d
sofiapltnva-glitch <sofiapltnva@gmail.com>
2026-09-17 16:01:54 +0700

    Add files via upload

--- LaboratoryWork01.cpp
+++ LaboratoryWork01.cpp
@@ -8,36 +8,37 @@
 #include <cmath>
 using namespace std;
 int main() {
-  double rho, compression, sigma, polytope;
-  cout << "rho = ";
-  cin >> rho;
-  
-  cout << "compression = ";
-  cin >> compression;
-  
-  cout << "sigma = ";
-  cin >> sigma;
-  
-  cout << "polytope = ";
-  cin >> polytope;
-  
-  double n;
-  n = (polytope - 1) / polytope;
-  
-  //with full regeneration
-  double n1;
-  n1 = 1 - (1 / rho);
-  
-  //with partial regeneration
-  double n2;
-  n2 = ((rho - 1) * (pow(compression, n) - 1)) / (pow(compression, n) * (rho - 1) - sigma * (rho - pow(compression, n)));
-  
-  //in the absence of regeneration
-  double n3;
-  n3 = 1 - (1 / pow(compression, n));
-  
-  
-  cout << "Efficiency with full regeneration = " << n1;
-  cout << "\nEfficiency with partial regeneration = " << n2;
-  cout << "\nEfficiency in the absence of regeneration = " << n3;
+	double rho, compression, sigma, polytope;
+
+	cout << "rho = ";
+	cin >> rho;
+
+	cout << "compression = ";
+	cin >> compression;
+
+	cout << "sigma = ";
+	cin >> sigma;
+
+	cout << "polytope = ";
+	cin >> polytope;
+
+	double n;
+	n = (polytope - 1) / polytope;
+
+	//with full regeneration
+	double n1;
+	n1 = 1 - (1 / rho);
+
+	//with partial regeneration
+	double n2;
+	n2 = ((rho - 1) * (pow(compression, n) - 1)) / (pow(compression, n) * (rho - 1) - sigma * (rho - pow(compression, n)));
+
+	//in the absence of regeneration
+	double n3;
+	n3 = 1 - (1 / pow(compression, n));
+
+
+	cout << "Efficiency with full regeneration = " << n1
+	<< "\nEfficiency with partial regeneration = " << n2
+	<< "\nEfficiency in the absence of regeneration = " << n3;
 }
